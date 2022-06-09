b63160d437a32f0697396abd916ae996f543c8e2
amuliawijaya23 <amuliawijaya@gmail.com>
2022-06-08 23:53:52 -0700

    update list item styling

--- client/src/components/Dashboard/ListItem.jsx
+++ client/src/components/Dashboard/ListItem.jsx
@@ -17,9 +17,22 @@ export default function ListItem(props) {
       <div className="list-item__image">
         <img src={props.cover_image_url} alt="cover" />
       </div>
-      Address: {props.street_address} {props.city} {props.province} {props.postal_code}
-      Type of Property: {props.property_type}
-      Bedrooms: {props.number_of_bedrooms} Bathrooms: {props.number_of_bathrooms} Parking: {props.parking_space} Size: {props.size} sqft - {props.status}
+      <div className="list-item__info">
+        <b>{props.title}</b>
+        <p>Address: {props.street_address} {props.city} {props.province} {props.postal_code}</p>
+      </div>
+      <div className="list-item__info">
+        <p>Property Type: {props.property_type}</p>
+        <p>Size: {props.size} sqft</p>
+      </div>
+      <div className="list-item__info">
+      <p>Bedrooms: {props.number_of_bedrooms}</p>
+      <p>Bathrooms: {props.number_of_bathrooms}</p>
+      </div>
+      <div className="list-item__info">
+      <p>Parking: {props.parking_space}</p>
+      <p>Status: {props.status}</p>
+      </div>
     </Item>
   );
 };
