432965448e6d1610137ee1f126e6a0a99bed07e6
amuliawijaya23 <amuliawijaya@gmail.com>
2022-06-14 16:16:39 -0700

    create react env for auth0 domain and client id

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,11 +5,14 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 import './styles.scss';
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
-    domain="dev-ptyxcp1a.au.auth0.com"
-    clientId="ZTBjJ2KsC9DEzaFlbW8cU91h2SOJghqO"
+    domain={domain}
+    clientId={clientID}
     redirectUri={window.location.origin}
   >
     <App />
