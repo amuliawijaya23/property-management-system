321f83858af9d3cf03e78d63dc08187cff32caa9
amuliawijaya23 <amuliawijaya@gmail.com>
2022-06-23 11:18:23 -0700

    deploy attempt 7

--- server/multer.js
+++ server/multer.js
@@ -1,13 +1,12 @@
 const multer = require('multer');
 
-// setup multer
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
+// const fileFilter = (req, file, cb) => {
+//   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+//     cb(null, true);
+//   } else {
+//     cb(null, false);
+//   }
+// };
 
 const upload = multer();
 
