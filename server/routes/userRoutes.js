aee78766bd4af686947e08a1f58ae98e2a5660d3
amuliawijaya23 <amuliawijaya@gmail.com>
2022-07-19 04:13:05 -0700

    adds login url

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -49,4 +49,43 @@ router.get('/role/:id', jwtCheck, async(req, res) => {
   }
 });
 
+router.get('/login', jwtCheck, async(req, res) => {
+  const token = await getManagementApiJwt();
+  const url = `${process.env.MANAGEMENT_API}/clients/Y8UigYR4SGqNwr50G1tvWm66b1sb5Yya`;
+  const headers = {
+    'content-type': 'application/json',
+    Authorization: `Bearer ${token.access_token}`,
+    'cache-control': 'no-cache'
+  };
+  const data = {
+    // eslint-disable-next-line camelcase
+    initiate_login_uri: 'https://my-property-management-system.herokuapp.com/user/login'
+  };
+
+  try {
+    const response = await axios.patch(url, { headers: headers, data: data});
+    console.log(response.data);
+  } catch (error) {
+    console.error(error.response ? error.response.body : error);
+  }
+});
+
+// const options = {
+//   method: 'PATCH',
+//   url: 'https://dev-ptyxcp1a.au.auth0.com/api/v2/clients/Y8UigYR4SGqNwr50G1tvWm66b1sb5Yya',
+//   headers: {
+//     'content-type': 'application/json',
+//     authorization: 'Bearer API2_ACCESS_TOKEN',
+//     'cache-control': 'no-cache'
+//   },
+//   // eslint-disable-next-line camelcase
+//   data: {initiate_login_uri: '<login_url>'}
+// };
+
+// axios.request(options).then(function (response) {
+//   console.log(response.data);
+// }).catch(function (error) {
+//   console.error(error);
+// });
+
 module.exports = router;
